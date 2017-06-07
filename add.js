ccbb827505203cd5e8995af909783e9d17391f7f
Adam Borowski <adam@adamboro.com>
2017-06-08 00:10:32 +0200

    add script - tweeting the added link

--- add.js
+++ add.js
@@ -1,9 +1,30 @@
+require('dotenv').config()
+
 const fs = require('fs')
 const indentString = require('indent-string')
 const R = require('ramda')
 const inquirer = require('inquirer')
 const YAML = require('yamljs')
 const validUrl = require('valid-url')
+const Twitter = require('twitter')
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
+
+const postTweet = status => {
+  client.post('statuses/update', {status}, function (error, tweet, response) {
+    if (error) {
+      console.log(error)
+      throw error
+    }
+    const tweetLink = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+    console.log('tweeted:', tweetLink)
+  })
+}
 
 const categories = YAML.load('./data/categories.yml')
 const links = YAML.load('./data/links.yml')
@@ -51,6 +72,11 @@ inquirer.prompt([
   },
   {
     type: 'confirm',
+    name: 'tweet',
+    message: 'tweet it'
+  },
+  {
+    type: 'confirm',
     name: 'confirm',
     message: 'looks legit?'
   }
@@ -59,10 +85,15 @@ inquirer.prompt([
     console.log('¯\\_(ツ)_/¯')
     return
   }
-  const link = R.dissoc('confirm', answers)
+  const link = R.omit(['confirm', 'tweet'], answers)
   const yamlString = `${indentString('-\n', 2)}${indentString(YAML.stringify(link, 4, 2), 4)}`
   fs.appendFile('./data/links.yml', yamlString, function (err) {
     if (err) throw err
     console.log('file saved 👏')
   })
+
+  if (answers.tweet) {
+    const tweet = `Here's a nice website: ${link.title} ${link.link}`
+    postTweet(tweet)
+  }
 })
